0918245668003e42fa453d901598057e658cf698
Haron-M <haronmoenga9@gmail.comm>
2026-07-02 12:21:24 +0300

    add envirnment variable

--- myLms/myLms/settings.py
+++ myLms/myLms/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,11 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# Tell Django to find static assets inside your apps
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'elearning_portal', 'static'),
-]
-
+# Tell Django to find static assets inside your cd 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
